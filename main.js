4ebfc5f473e9b50b41f6c5b108dcf7cd2af90782
sahar-cohen <saharc21@gmail.com>
2023-05-17 16:45:46 +0300

    flipped order and added reset css

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ function updateCountdown() {
   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-  document.getElementById("ימים").innerText = days;
-  document.getElementById("שעות").innerText = hours;
-  document.getElementById("דקות").innerText = minutes;
   document.getElementById("שניות").innerText = seconds;
+  document.getElementById("דקות").innerText = minutes;
+  document.getElementById("שעות").innerText = hours;
+  document.getElementById("ימים").innerText = days;
 }
 
 updateCountdown();
