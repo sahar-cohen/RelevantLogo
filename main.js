34c393c45db01a612c702bb7a7228f7c74f120c2
sahar-cohen <saharc21@gmail.com>
2023-05-18 15:01:07 +0300

    changing every 30 minutes

--- main.js
+++ main.js
@@ -13,11 +13,36 @@ function updateCountdown() {
   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-  document.getElementById("ימים").innerText = days;
-  document.getElementById("שעות").innerText = hours;
-  document.getElementById("דקות").innerText = minutes;
   document.getElementById("שניות").innerText = seconds;
+  document.getElementById("דקות").innerText = minutes;
+  document.getElementById("שעות").innerText = hours;
+  document.getElementById("ימים").innerText = days;
 }
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+
+// Theme switching code
+let isDay = true;
+
+function setWebsiteTheme() {
+  const body = document.body; // Use body directly
+  // Clear any previous theme classes
+  body.className = "";
+
+  // Switch between day and night every 30 seconds
+  if (isDay) {
+    body.classList.add("night");
+  } else {
+    body.classList.add("day");
+  }
+
+  // Flip the isDay flag for the next run
+  isDay = !isDay;
+}
+
+// Call the function once at the start to set the initial theme
+setWebsiteTheme();
+
+// Then call the function every 30 seconds to check and update the theme
+setInterval(setWebsiteTheme, 1000 * 30);
