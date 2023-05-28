6dbcba01caac0413077a9cc77cdc0dfc8d389328
sahar-cohen <saharc21@gmail.com>
2023-05-28 12:22:36 +0300

    change to 30 min

--- main.js
+++ main.js
@@ -51,5 +51,5 @@ function setWebsiteTheme() {
 // Call the function once at the start to set the initial theme
 setWebsiteTheme();
 
-// Then call the function every 30 seconds to check and update the theme
-setInterval(setWebsiteTheme, 1000 * 60); // 30 seconds
+// Then call the function every 30 minutes to check and update the theme
+setInterval(setWebsiteTheme, 1000 * 60 * 30); // 30 minutes
